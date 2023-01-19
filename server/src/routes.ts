5c9aa04b4faa3231604b231e2ca843de8d128b4f
viniciusvilar <61420617+viniciusvilar@users.noreply.github.com>
2023-01-19 14:28:10 -0300

    Criando rota patch

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -69,8 +69,58 @@ export async function appRoutes(app: FastifyInstance) {
 
         return {
             possibleHabits,
-            completeHabits
+            completeHabits,
         }
     })
+
+    app.patch('/habits/:id/toggle', async (request) => {
+        const toggleHabitParams = z.object({
+            id: z.string().uuid()
+        })
+
+        const { id } = toggleHabitParams.parse(request.params)
+
+        const today = dayjs().startOf('day').toDate()
+
+        let day = await prisma.day.findUnique({
+            where: {
+                date: today
+            }
+        })
+
+        if (!day) {
+            day = await prisma.day.create({
+                data: {
+                    date: today
+                }
+            })
+        }
+
+        const dayHabit = await prisma.dayHabit.findUnique({
+            where: {
+                day_id_habit_id: {
+                    day_id: day.id,
+                    habit_id: id
+                }
+            }
+        })
+
+        if (dayHabit) {
+            await prisma.dayHabit.delete({
+                where: {
+                    id: dayHabit.id
+                }
+            })
+        } else {
+            await prisma.dayHabit.create({
+                data: {
+                    day_id: day.id,
+                    habit_id: id
+                }
+            })
+        }
+    })
+
+    
 }
 
