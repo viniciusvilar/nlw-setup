4ac41a0b5e63520163ac4ed6e208aad1c159d6a4
viniciusvilar <61420617+viniciusvilar@users.noreply.github.com>
2023-01-19 13:34:31 -0300

    Aula 02

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,20 @@
-import ''
+import './styles/global.css'
+import logoImage from './assets/logo.svg'
+import { Plus } from 'phosphor-react'
+import { Header } from './components/Header'
+import { SummaryTable } from './components/SummaryTable'
 
-function App() {
+export function App() {
   return (
-    <h1>Hello World</h1>
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+        
+        <Header />
+        <SummaryTable />
+
+      </div>
+
+    </div>
   )
 }
 
-export default App
